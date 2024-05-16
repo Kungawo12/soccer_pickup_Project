bc56daf7dc1350efd1cff5ba82969349683bded6
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2024-05-16 18:38:11 -0500

    created home page

--- pickup_project/controllers/users.py
+++ pickup_project/controllers/users.py
@@ -1,2 +1,6 @@
 from flask import render_template,request, redirect,session,flash
 from pickup_project import app
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
--- server.py
+++ server.py
@@ -1,2 +1,5 @@
 from pickup_project import app
 from pickup_project.controllers import users
+
+if __name__ == '__main__':
+    app.run(debug=True, port= 5001)
