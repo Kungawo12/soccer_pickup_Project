93a585712835a90bb32c4fc711a2a251b1942fa3
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2024-05-20 11:32:57 -0500

    created user controller and model

--- pickup_project/controllers/users.py
+++ pickup_project/controllers/users.py
@@ -1,6 +1,43 @@
 from flask import render_template,request, redirect,session,flash
 from pickup_project import app
+from pickup_project.models.user import User
 
 @app.route('/')
 def home_page():
     return render_template('home.html')
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
+
+@app.route('/user/register', methods=['POST'])
+def register_user():
+    if not User.validate_user(request.form):
+        return redirect('/register')
+    
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": request.form['password']
+    }
+    user_id = User.insert(data)
+    session['user_id'] = user_id
+    return redirect('/dashboard')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/user/login', methods=['POST'])
+def login_user():
+    data = {
+        "email": request.form['email'],
+        "password": request.form['password']
+    }
+    user = User.get_user(data)
+    if not user:
+        flash("Invalid credentials")
+        return redirect('/login')
+    session['user_id'] = user.id
+    return redirect('/dashboard')
---
+++ pickup_project/models/user.py
@@ -0,0 +1,32 @@
+from pickup_project.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+class User:
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        
+    @classmethod
+    def get_all_users(cls):
+        query= "SELECT * FROM users"
+        results = connectToMySQL('pickup_project').query_db(query)
+        users = []
+        
+        for user in results:
+            users.append(cls(user))
+            
+        return users
+
+    @classmethod
+    def insert(cls,data):
+        query = """INSERT INTO users(first_name,last_name,email,password)
+            VALUES(%(first_name)s, %(last_name)s, %(email)s, %(password)s);
+        """
+        return connectToMySQL('pickup_project').query_db(query,data)
